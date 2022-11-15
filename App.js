c605eeca2cbe62f7d8fce2d3b6c0e1940f28dd22
layobright <alayosingers@gmail.com>
2022-11-15 05:03:31 -0800

    installed neccessary dependencies and project folder setup

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './assets/colors/colors';
+import { AntDesign } from '@expo/vector-icons';
+
+
 
 export default function App() {
+  // <AntDesign name="delete" size={24} color="black" />
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontWeight:'bold'}}>FOODIE</Text>
     </View>
   );
 }
@@ -16,5 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize:50,
   },
 });
---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    background:'#F9F9FB',
+    textDark:'#313234',
+    primary:'#f5ca48',
+    secondary:'#f26c68',
+    textLight:'#cdcdcd',
+    price:'#e4723c'
+}
+
+export default colors
